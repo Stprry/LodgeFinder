e55145080ea58a70db444b687102746b7ac54ced
Stprry <stprry96@gmail.com>
2020-10-12 20:31:45 +0100

    Added ability to get users current location co-ordinats, and show users dot on map

---
+++ SurflineCodingTest/ViewControllers/LandingViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  SurflineCodingTest
+//
+//  Created by Sam Perry on 12/10/2020.
+//  Copyright © 2020 Sam Perry. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class LandingViewController: UIViewController,MKMapViewDelegate{
+
+    @IBOutlet weak var SearchBar: UISearchBar!
+    @IBOutlet weak var CurrentLocationButton: UIButton!
+    @IBOutlet weak var SearchButton: UIButton!
+    @IBOutlet weak var ClearButton: UIButton!
+    @IBOutlet weak var MapView: MKMapView!
+    let reigonKM: Double = 1000
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MapView.delegate = self
+        checkLocationAuth()
+        MapView.showsUserLocation = true // show location on map.
+
+    }
+    //MARK: -- Button Actions
+    @IBAction func CurrentLocationBtn(_ sender: Any) {
+        let center = getCenterLocation(for: MapView)
+        let lat: String = String(format:"%f", center.coordinate.latitude)
+        let long: String = String(format: "%f", center.coordinate.longitude)
+        SearchBar.placeholder = "\(lat) \(long)"
+    }
+    
+    //MARK: -- Location Set up and authorisation
+    func locationManagerSetUp(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    func checkLocationServices(){
+        if  CLLocationManager.locationServicesEnabled(){
+            locationManagerSetUp()
+        }else{
+           //show alert
+        }
+    }
+    
+    func checkLocationAuth(){
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse: // the main usage will be when in use so the bulk of code will be here.
+            MapView.showsUserLocation = true // show location on map.
+            centerViewOnUser() // zoom in to 1km of the user.
+            locationManager.startUpdatingLocation() // start updating location incase the user moves.
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization() // request permissions
+            break
+        case .restricted:
+           // display alert instructing them we need permissions
+            break
+        case .denied:
+           // display alert instructing them we need permissions
+            break
+        case .authorizedAlways:// We arn't doing anythign with this beacuse we arn't requesting it
+            break
+        @unknown default:
+            print("unknown auth status")
+        }
+    }
+    func centerViewOnUser(){
+        if let location = locationManager.location?.coordinate {
+            let fetchReigon = MKCoordinateRegion.init(center: location, latitudinalMeters: reigonKM,longitudinalMeters: reigonKM)
+            MapView.setRegion(fetchReigon, animated: true)
+        }
+    }
+    
+    func getCenterLocation(for MapView:MKMapView) -> CLLocation {
+        let lat = MapView.centerCoordinate.latitude
+        let long = MapView.centerCoordinate.longitude
+        return CLLocation(latitude: lat, longitude: long)
+    }
+    
+}
+//MARK: -- Location Manager Delegates
+extension LandingViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //update locations
+        guard let location = locations.last else {return} // if last locations is nil, do nothing
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)// get users location from the location co-ordinates
+        let reigon = MKCoordinateRegion(center: center, latitudinalMeters: reigonKM, longitudinalMeters: reigonKM)//create reigon 1km wide, with the user at center
+        MapView.setRegion(reigon, animated: true)
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // check user change auth
+        checkLocationAuth()
+    }
+}
+
--- SurflineCodingTest/ViewControllers/ListViewController.swift
+++ SurflineCodingTest/ViewControllers/ListViewController.swift
@@ -1,20 +1,21 @@
 //
-//  ViewController.swift
+//  ListViewController.swift
 //  SurflineCodingTest
 //
 //  Created by Sam Perry on 12/10/2020.
 //  Copyright © 2020 Sam Perry. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ListViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
+
 
